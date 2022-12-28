2cbfb28e68f647610528f81f2f9bc22c24809e64
Adrian Gutierrez Gomez <adriangtzg.reverie@gmail.com>
2022-12-27 18:14:57 -0600

    Add game logic

---
+++ javascript.js
@@ -0,0 +1,80 @@
+        let playerRecord = 0
+        let computerRecord = 0
+        let tieRecord = 0
+        let stringVerify1 = "ROCK"
+        let stringVerify2 = "PAPER"
+        let stringVerify3 = "SCISSORS"
+
+        function getPlayerChoice(){
+        let a = prompt("Choose rock, paper or scissors. Please")
+        a = a.toUpperCase()
+        while (!(["ROCK", "PAPER", "SCISSORS"].includes(a))){
+            a = prompt("Didn't recognize that, please try again.")
+            a = a.toUpperCase()
+        }
+        return a
+        }      
+
+        function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+        }
+        
+        function getComputerChoice() {
+           let a = getRndInteger(1,3)
+        if (a == 1){
+            a = "ROCK"
+        }
+        else if(a == 2){
+            a = "PAPER"
+        }
+        else {
+        a = "SCISSORS"}
+        return a
+        }
+
+        function playRound(playerChoice, computerChoice) {
+            console.log(`You chose: ${playerChoice}`)
+            console.log(`Computer chose: ${computerChoice}`)
+            
+            if ((playerChoice == "ROCK" && computerChoice == "SCISSORS")||(playerChoice == "PAPER" && computerChoice == "ROCK")||(playerChoice == "SCISSORS" && computerChoice == "PAPER")){
+                console.log("You win the round!")
+                playerRecord++
+                return playerRecord;
+            }
+            
+       
+            else if ((playerChoice == "ROCK" && computerChoice == "PAPER")||(playerChoice == "PAPER" && computerChoice == "SCISSORS")||(playerChoice == "SCISSORS" && computerChoice == "ROCK")){
+                console.log("You lose the round!")
+                computerRecord++
+                return computerRecord;
+            }
+            
+            
+            else if ((playerChoice == "ROCK" && computerChoice == "ROCK")||(playerChoice == "PAPER" && computerChoice == "PAPER")||(playerChoice == "SCISSORS" && computerChoice == "SCISSORS")){
+                tieRecord++
+                console.log("It is a tie!!")
+                return tieRecord;
+            }     
+
+    
+    }
+
+    function game(){
+        for (let i = 0; i < 5; i++) {
+            let playerChoice = getPlayerChoice()
+            let computerChoice = getComputerChoice()
+            playRound(playerChoice, computerChoice)
+
+    }
+    if (playerRecord > computerRecord){
+        console.log("You win the game!!!")
+    }
+    else {
+        console.log("You lose the game!!!")
+    }
+    let gameStats = `You: ${playerRecord} - Computer: ${computerRecord} - Ties ${tieRecord}` 
+         console.log(gameStats)
+    }
+       
+        game()
+        
